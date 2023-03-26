e685b41247736c6ec819dd5fcc39bb5e030b34f9
soraLTQN <112687052+ltqnkhtn12@users.noreply.github.com>
2023-03-26 22:05:01 +0700

    Add files via upload

---
+++ bai5.cpp
@@ -0,0 +1,48 @@
+﻿//Xây dựng lớp đối tượng người (Person) với các thuộc tính tên, tuổi và địa chỉ
+//và các phương thức nhập và xuất thông tin người
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+private:
+	string name;
+	int age;
+	string address;
+public:
+	Person(string name, int age, string address) {
+		this->name = name;
+		this->age = age;
+		this->address = address;
+	}
+	string getName() const{
+		return name;
+	}
+	void setName(string name) {
+		this->name = name;
+	}
+	int getAge() const {
+		return age;
+	}
+	void setAge(int age) {
+		this->age = age;
+	}
+	string getAddress() {
+		return address;
+	}
+	void setAddress(string address) {
+		this->address = address;
+	}
+	void display() const {
+		cout << "Name: " << name << endl;
+		cout << "Age: " << age << endl;
+		cout << "Address: " << address << endl;
+	}
+};
+//Kiểm tra kết quả bằng cách tạo đối tượng Person và hiển thị thông tin của nó
+int main() {
+	Person person("Nhu", 23, "Thanh pho Ho Chi Minh");
+    person.display();
+	return 0;
+}
