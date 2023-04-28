31c55456c550b395dd0b3e4c3ba8328aa9f996b9
soraLTQN <112687052+ltqnkhtn12@users.noreply.github.com>
2023-04-29 02:16:19 +0700

    Add files via upload

---
+++ Point.cpp
@@ -0,0 +1,31 @@
+class Point {
+private:
+	int x;
+	int y;
+
+public:
+	Point() {
+		
+	}
+
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+
+	int getX() {
+		return x;
+	}
+
+	void setX(int x) {
+		this->x = x;
+	}
+
+	int getY() {
+		return y;
+	}
+
+	void setY(int y) {
+		this->y = y;
+	}
+};
